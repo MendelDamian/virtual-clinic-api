bce2fbf7c7cc2da9aa08d73ad10426288168b770
Damian Mendel <mendeldamian01@gmail.com>
2022-11-15 18:23:30 +0100

    Added user routes, user model and session, registrations controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      devise_for :users, controllers: {
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations'
+      }
     end
   end
 end
