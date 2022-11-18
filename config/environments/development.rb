cfc82f594fc7ecf19c810da5d37f15ba7a52d759
Damian Mendel <mendeldamian01@gmail.com>
2022-11-18 12:30:49 +0100

    delete session storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.middleware.use ActionDispatch::Cookies
-  config.middleware.use ActionDispatch::Session::CookieStore, key: '_namespace_key'
-
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
