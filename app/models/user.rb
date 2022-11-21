6091d4aed33a794071ce551536771741fb611a00
Mishu5 <michalgostkow@gmail.com>
2022-11-21 17:13:42 +0100

    add denylist revocation strategy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
