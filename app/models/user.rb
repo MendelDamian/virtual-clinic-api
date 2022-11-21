5ba231afff3463389269473cbdafd460d01a20ec
Mishu5 <michalgostkow@gmail.com>
2022-11-21 17:31:31 +0100

    restore registerable and validatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
