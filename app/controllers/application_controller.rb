5a0e3cfb8b793fec1899b7785f33ad22bd1f4b30
Damian Mendel <mendeldamian01@gmail.com>
2022-11-22 13:06:52 +0100

    add signing out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   before_action :update_allowed_parameters, if: :devise_controller?
   respond_to :json
 
