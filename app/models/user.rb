89fd9a6fca601328b8debdaa341e5f821106ef77
Damian Mendel <mendeldamian01@gmail.com>
2022-11-25 10:52:00 +0100

    throw 422 on invalid account_type passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
     patient: 0,
     doctor: 1
   }, _prefix: true
+
+  validates_inclusion_of :account_type, in: account_types.keys, message: 'is not a valid account_type'
+
+  def account_type=(value)
+    self[:account_type] = value
+  rescue ArgumentError
+    self[:account_type] = nil
+  end
 end
