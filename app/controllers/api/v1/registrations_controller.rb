0648a3064fce7b5315adf40d5c42d72fb21f6225
Damian Mendel <mendeldamian01@gmail.com>
2022-11-30 16:06:18 +0100

    allow doctor to update his professions

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,10 +4,31 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
       if resource.persisted? && resource.account_type_doctor?
         params_professions = params[:user][:professions]
 
-        # For now I am skipping not found professions.
         professions_db = Profession.where(name: params_professions)
         professions_db.each do |profession|
-          profession.user_professions << UserProfession.new(user_id: resource.id)
+          profession.user_professions.create!(user_id: resource.id)
+        end
+      end
+    end
+  end
+
+  def update
+    super do |resource|
+      if resource.persisted? && resource.account_type_doctor?
+        params_professions = params[:user][:professions]
+
+        # If professions are not sent, then do nothing. It's recommended to do not send a param if it's not modified.
+        if params_professions.nil?
+          return
+        end
+
+        # If so, then delete all professions for this doctor.
+        UserProfession.where(user_id: resource.id).destroy_all
+
+        # And add the new ones.
+        professions_db = Profession.where(name: params_professions)
+        professions_db.each do |profession|
+          profession.user_professions.create!(user_id: resource.id)
         end
       end
     end
