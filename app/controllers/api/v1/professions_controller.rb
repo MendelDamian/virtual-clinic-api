0db5e2cb1d4ff95a8173da78a91892b635240058
Damian Mendel <mendeldamian01@gmail.com>
2022-11-30 15:38:47 +0100

    remove param permit and add params validation

--- app/controllers/api/v1/professions_controller.rb
+++ app/controllers/api/v1/professions_controller.rb
@@ -3,6 +3,12 @@ class Api::V1::ProfessionsController < Api::V1::ApplicationController
   PROFESSIONS_PER_PAGE = 10
 
   def index
+    # Validate pagination params.
+    if params[:page].to_i < STARTING_PAGE || params[:per_page].to_i < 1
+      render json: { error: 'Invalid page or per_page' }, status: :bad_request
+      return
+    end
+
     default_pagination_params = { page: STARTING_PAGE, per_page: PROFESSIONS_PER_PAGE }
     params.reverse_merge!(default_pagination_params)
     @professions = Profession.where('name LIKE ?', "%#{params[:name]}%")
@@ -14,9 +20,9 @@ class Api::V1::ProfessionsController < Api::V1::ApplicationController
   # Display all professions for a doctor.
   # GET /api/v1/professions/doctor/:doctor_id
   def doctor
-    doctor = Doctor.where(id: params[:doctor_id]).first
-    if doctor
-      render json: doctor.professions, status: :ok
+    @doctor = Doctor.where(id: params[:doctor_id]).first
+    if @doctor
+      render json: @doctor.professions, status: :ok
     else
       render json: { error: 'Doctor not found' }, status: :not_found
     end
@@ -35,6 +41,5 @@ class Api::V1::ProfessionsController < Api::V1::ApplicationController
 
   def profession_params
     params.require(:profession).permit(:name)
-    params.permit(:page, :per_page)
   end
 end
