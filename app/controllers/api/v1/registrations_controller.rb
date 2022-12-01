4c46805503cddf9b20fcf9b790a8455123d99ea1
Damian Mendel <mendeldamian01@gmail.com>
2022-12-01 17:09:12 +0100

    export redundant functionality to exterior method

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,38 +1,37 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
-      if resource.persisted? && resource.account_type_doctor?
-        params_professions = params[:user][:professions]
+      return unless resource.persisted? && resource.account_type_doctor?
 
-        entries = []
-        Profession.where(name: params_professions).find_each do |profession|
-          entries << { user_id: resource.id, profession_id: profession.id }
-        end
-
-        UserProfession.insert_all(entries)
-      end
+      params_professions = params[:user][:professions]
+      add_professions_to_doctor(resource, params_professions) if params_professions
     end
   end
 
   def update
     super do |resource|
-      if resource.persisted? && resource.account_type_doctor?
-        params_professions = params[:user][:professions]
+      return unless resource.persisted? && resource.account_type_doctor?
 
-        # If professions are not sent, then do nothing. It's recommended to do not send this param if it's not modified.
-        return if params_professions.nil?
+      # If professions are not sent, then do nothing. It's recommended to do not send this param if it's not modified.
+      params_professions = params[:user][:professions]
+      return if params_professions.nil?
 
-        # If so, then delete all professions for this doctor.
-        UserProfession.delete_by(user_id: resource.id)
+      # If so, then delete all professions for this doctor.
+      UserProfession.delete_by(user_id: resource.id)
 
-        # And add the new ones.
-        entries = []
-        Profession.where(name: params_professions).find_each do |profession|
-          entries << { user_id: resource.id, profession_id: profession.id }
-        end
-
-        UserProfession.insert_all(entries)
-      end
+      # And add the new ones.
+      add_professions_to_doctor(resource, params_professions)
     end
+  end
+
+  private
+
+  def add_professions_to_doctor(doctor, params_professions)
+    entries = []
+    Profession.where(name: params_professions).find_each do |profession|
+      entries << { user_id: doctor.id, profession_id: profession.id }
+    end
+
+    UserProfession.insert_all(entries)
   end
 end
