b400358cd4199237f0749205dcc20baeb87e57db
Wiktor Heblewski <95719616+Kaysaar@users.noreply.github.com>
2022-12-01 21:54:24 +0100

    split relation and validations
    Co-authored-by: Damian Mendel <mendeldamian01@gmail.com>

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,5 +1,6 @@
 class Procedure < ApplicationRecord
   belongs_to :user, inverse_of: :procedures, foreign_key: :users_id
+  
   validates_uniqueness_of :name, scope: :users_id
   validates :name, length: { minimum: 2 , maximum: 128}
   validates :needed_time, presence: true
