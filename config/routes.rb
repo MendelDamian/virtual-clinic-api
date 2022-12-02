66571322db8a409661287b74716bb156882560da
Wiktor Heblewski <95719616+Kaysaar@users.noreply.github.com>
2022-12-02 13:22:38 +0100

    Update route of procedure
    Co-authored-by: Damian Mendel <mendeldamian01@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, defaults: { format: :json }
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :procedures
     end
