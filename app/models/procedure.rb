1f4234b01b245323dff38fe85ad12e4224d798e9
Wiktor Heblewski <240681@edu.p.lodz.pl>
2022-12-02 13:36:13 +0100

    Private validation of procedure's needed time

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,19 +1,19 @@
-class TimeLengthValidator <  ActiveModel::Validator
-
-  def validate(record)
-    shortest_procedure = "00:20"
-    if record.needed_time.strftime('%H:%M') < shortest_procedure
-      record.errors.add :base, "The time of procedure is too short"
-    end
-  end
-
-end
-
 class Procedure < ApplicationRecord
   belongs_to :user, inverse_of: :procedures, foreign_key: :users_id
   
   validates_uniqueness_of :name, scope: :users_id
   validates :name, length: { minimum: 2 , maximum: 128}
   validates :needed_time, presence: true
-  validates_with TimeLengthValidator
+
+  validate :time_length_validation
+
+  private
+
+  def time_length_validation(record)
+    shortest_procedure = "00:20"
+
+    if record.needed_time.strftime('%H:%M') < shortest_procedure
+      record.errors.add :base, "The time of procedure is too short"
+    end
+  end
 end
