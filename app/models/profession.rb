239d23715b661aeed7b2f6127fef9b271643d3a9
Damian Mendel <mendeldamian01@gmail.com>
2022-12-04 13:50:49 +0100

    add filterable concern

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,10 +1,12 @@
 class Profession < ApplicationRecord
+  include Filterable
+
   has_many :user_professions, dependent: :destroy
   has_many :users, through: :user_professions
 
-  # Filters
+  # Scopes.
   scope :filter_by_name, -> (name) { where('name LIKE ?', "%#{name}%") }
 
-  # Validations
+  # Validations.
   validates :name, length: { maximum: 50, minimum: 2 }, uniqueness: true
 end
