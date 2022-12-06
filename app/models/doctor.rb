722131d497ddda749379e8900aa72009935529bd
Damian Mendel <mendeldamian01@gmail.com>
2022-12-06 14:35:29 +0100

    delete unnecessary comment

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,6 @@
 class Doctor < User
   default_scope { where(account_type: :doctor) }
 
-  # ERROR: update or delete on table "professions" violates foreign key constraint "fk_rails_7715598ad9" on table "user_professions" (PG::ForeignKeyViolation)
   has_many :user_professions, foreign_key: :user_id, dependent: :delete_all
   has_many :professions, through: :user_professions
 end
