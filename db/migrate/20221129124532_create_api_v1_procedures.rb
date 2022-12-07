d4d45c3fe557e839b474381bfc8b62a409780fcf
Wiktor Heblewski <240681@edu.p.lodz.pl>
2022-12-07 14:49:22 +0100

    Changed procedures table

--- db/migrate/20221129124532_create_api_v1_procedures.rb
+++ db/migrate/20221129124532_create_api_v1_procedures.rb
@@ -2,10 +2,10 @@ class CreateApiV1Procedures < ActiveRecord::Migration[6.1]
   def change
     create_table :procedures do |t|
       t.belongs_to :user, foreign_key: true
-      t.string :name
-      t.integer :needed_time_min
+      t.string :name, null: false
+      t.integer :needed_time_min, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
