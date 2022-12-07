4d3b7307129628a7d0a5e622c5c60ce5540fc03f
Wiktor Heblewski <240681@edu.p.lodz.pl>
2022-12-07 15:01:20 +0100

    decreased length of code line

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -6,7 +6,8 @@ class Procedure < ApplicationRecord
 
   validates_uniqueness_of :name, scope: :user_id
   validates :name, length: { minimum: 2 , maximum: 128}
-  validates :needed_time_min, presence: true, numericality: {greater_than_or_equal_to: 20,less_than_or_equal_to: 7200, message: 'is incorrect' }
+  validates :needed_time_min, presence: true, numericality:
+    {greater_than_or_equal_to: 20,less_than_or_equal_to: 7200, message: 'is incorrect' }
 
 end
 
