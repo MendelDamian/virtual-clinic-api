6a7d039bdf3bec9b93b66d8a705e2b9cb41ca1a5
Wiktor Heblewski <240681@edu.p.lodz.pl>
2022-12-13 13:42:37 +0100

    feat: Added endpoint to list of work_plans of specific doctor

---
+++ app/controllers/api/v1/doctors/work_plans_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::Doctors::WorkPlansController < Api::V1::ApplicationController
+  include ApiResponse
+
+  def index
+    json_response
+  end
+
+  def set_collection
+    @collection = Doctor.find(params[:doctor_id]).work_plans.order(:day_of_week)
+  end
+
+  def filtering_params
+    params.slice()
+  end
+end
