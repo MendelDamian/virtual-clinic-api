5acba0b2cb90a3f68fdd9322a3df850a7148eb9b
Damian Mendel <mendeldamian01@gmail.com>
2022-12-14 15:40:35 +0100

    add filtering appointments by date

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
+  include Filterable
+
   belongs_to :doctor, inverse_of: :appointments, foreign_key: :doctor_id
   belongs_to :patient, inverse_of: :appointments, foreign_key: :patient_id
   belongs_to :procedure, inverse_of: :appointments, foreign_key: :procedure_id
@@ -9,6 +11,14 @@ class Appointment < ApplicationRecord
     canceled: 2
   }, _prefix: 'status'
 
+  # Scopes.
+  scope :filter_by_start_time, -> (start_time) {
+    where("start_time BETWEEN ? AND ?",
+          "#{start_time.beginning_of_day}",
+          "#{start_time.end_of_day}"
+    ) if start_time.present? }
+
+  # Validations.
   validates_inclusion_of :status, in: statuses.keys, message: 'is not a valid status'
   validates :start_time, presence: true
 
