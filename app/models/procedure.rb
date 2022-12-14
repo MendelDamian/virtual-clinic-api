635030ddc3684a6bb5f391597ee76efdd0d24eb8
Damian Mendel <mendeldamian01@gmail.com>
2022-12-14 14:30:52 +0100

    extract numerical constants

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,12 +1,21 @@
 class Procedure < ApplicationRecord
   include Filterable
+
+  SHORTEST_PROCEDURE_TIME = 20
+  LONGEST_PROCEDURE_TIME = 20
+
   belongs_to :doctor, inverse_of: :procedures, foreign_key: :user_id
 
+  # Scopes.
   scope :filter_by_name, -> (name) { where("name LIKE ?", "%#{name}%") if name.present? }
 
+  # Validations.
   validates_uniqueness_of :name, scope: :user_id
   validates :name, length: { minimum: 2, maximum: 128 }
-  validates :needed_time_min, presence: true, numericality:
-    { greater_than_or_equal_to: 20, less_than_or_equal_to: 7200, message: 'is incorrect' }
+  validates :needed_time_min, presence: true, numericality: {
+    greater_than_or_equal_to: SHORTEST_PROCEDURE_TIME,
+    less_than_or_equal_to: LONGEST_PROCEDURE_TIME,
+    message: 'is incorrect'
+  }
 end
 
