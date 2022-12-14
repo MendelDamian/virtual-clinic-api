5f1bbec201a598aad4a8290b6db2609efb9e6146
Damian Mendel <mendeldamian01@gmail.com>
2022-12-14 21:40:27 +0100

    extract logic to service

--- app/controllers/api/v1/procedures/appointments_controller.rb
+++ app/controllers/api/v1/procedures/appointments_controller.rb
@@ -1,57 +1,19 @@
 class Api::V1::Procedures::AppointmentsController < Api::V1::ApplicationController
   before_action :date_param_valid?, only: [:availability]
-  before_action :set_vars, only: %i[availability]
+  before_action :set_procedure, only: %i[availability]
 
   INVALID_DATE_ERROR = { "date": ["is invalid"] }
 
   # GET /api/v1/procedures/:procedure_id/appointments/availability?date=
   def availability
-    # While iterating I will use number of minutes from the beginning of the day.
-
-    # TODO: move this to an external service to do not bloat the controller.
-    time_curr = minutes(@work_plan.work_hour_start)
-    time_end = minutes(@work_plan.work_hour_end)
-
-    available_slots = []
-    while time_curr + @procedure.needed_time_min <= time_end
-      skip = false
-      @appointments.each do |appointment|
-        if between?(time_curr + @procedure.needed_time_min, appointment)
-          skip = true
-          time_curr = datetime_to_minutes(appointment.start_time) + appointment.procedure.needed_time_min
-          break
-        end
-      end
-
-      next if skip
-
-      available_slots << Time.parse("#{time_curr / 60}:#{time_curr % 60}").strftime("%H:%M")
-      time_curr += Procedure::SHORTEST_PROCEDURE_TIME
-    end
-
+    available_slots = AppointmentsService.available_slots(@procedure, @date)
     render json: { data: available_slots }, status: :ok
   end
 
   private
 
-  def between?(time_curr, appointment)
-    time_curr > datetime_to_minutes(appointment.start_time) &&
-      time_curr < datetime_to_minutes(appointment.start_time) + appointment.procedure.needed_time_min
-  end
-
-  def datetime_to_minutes(datetime)
-    datetime.hour * 60 + datetime.min
-  end
-
-  def minutes(hours)
-    hours * 60
-  end
-
-  def set_vars
+  def set_procedure
     @procedure = Procedure.find(params[:procedure_id].to_i)
-    @doctor = @procedure.doctor
-    @work_plan = @doctor.work_plans.find_by(day_of_week: @date.wday) # FIXME
-    @appointments = @doctor.appointments.filter_by_start_time(@date)
   end
 
   def param_date
---
+++ app/services/appointments_service.rb
@@ -0,0 +1,44 @@
+class AppointmentsService
+  def self.available_slots(procedure, date)
+    doctor = procedure.doctor
+    work_plan = doctor.work_plans.find_by(day_of_week: date.wday)
+    appointments = doctor.appointments.filter_by_start_time(date)
+    
+    time_curr = minutes(work_plan.work_hour_start)
+    time_end = minutes(work_plan.work_hour_end)
+
+    available_slots = []
+    while time_curr + procedure.needed_time_min <= time_end
+      skip = false
+      appointments.each do |appointment|
+        if between?(time_curr + procedure.needed_time_min, appointment)
+          skip = true
+          time_curr = datetime_to_minutes(appointment.start_time) + appointment.procedure.needed_time_min
+          break
+        end
+      end
+
+      next if skip
+
+      available_slots << Time.parse("#{time_curr / 60}:#{time_curr % 60}").strftime("%H:%M")
+      time_curr += Procedure::SHORTEST_PROCEDURE_TIME
+    end
+
+    available_slots
+  end
+
+  private
+
+  def self.between?(time_curr, appointment)
+    time_curr > datetime_to_minutes(appointment.start_time) &&
+      time_curr < datetime_to_minutes(appointment.start_time) + appointment.procedure.needed_time_min
+  end
+
+  def self.datetime_to_minutes(datetime)
+    datetime.hour * 60 + datetime.min
+  end
+
+  def self.minutes(hours)
+    hours * 60
+  end
+end
