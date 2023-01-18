ec155bbfc481bb98b4730a0c166d00adee4aea17
Damian Mendel <mendeldamian01@gmail.com>
2023-01-18 12:43:42 +0100

    add patient and doctor factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,14 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    account_type { User.account_types.keys.sample }
+  end
+
+  factory :patient, parent: :user do
+    account_type { :patient }
+  end
+
+  factory :doctor, parent: :user do
+    account_type { :doctor }
   end
 end
