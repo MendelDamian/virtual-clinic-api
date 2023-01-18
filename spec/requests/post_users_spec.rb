04c0e6adbb687bb9f1320fa26d0838dd7380ccce
Damian Mendel <mendeldamian01@gmail.com>
2023-01-18 14:19:38 +0100

    add tests for assigning professions on register

--- spec/requests/post_users_spec.rb
+++ spec/requests/post_users_spec.rb
@@ -123,9 +123,138 @@ RSpec.describe "Users", type: :request do
         user = User.last
         expect(response.body).to eq(user.to_json)
       end
-      
+
       it "creates a session with the last created user" do
         expect(controller.current_user).to eq(User.last)
+      end
+    end
+
+    context "as a doctor" do
+      context "with valid profession" do
+        let(:profession) { FactoryBot.create(:profession) }
+        let(:user_attributes) { FactoryBot.attributes_for(:doctor, professions: [profession.name]) }
+
+        before do
+          post "/users", params: { user: user_attributes }
+        end
+
+        it "returns a 201 status code" do
+          expect(response).to have_http_status(201)
+        end
+
+        it "assigns the profession to the doctor" do
+          expect(Doctor.last.professions).to eq([profession])
+        end
+
+        it "returns the correct json" do
+          expect(response.body).to eq(User.last.to_json)
+        end
+
+        it "creates a session with the last created user" do
+          expect(controller.current_user).to eq(User.last)
+        end
+      end
+
+      context "with invalid profession" do
+        let(:invalid_profession) { "invalid_profession" }
+        let(:user_attributes) { FactoryBot.attributes_for(:doctor, professions: [invalid_profession]) }
+
+        before do
+          post "/users", params: { user: user_attributes }
+        end
+
+        it "returns a 201 status code" do
+          expect(response).to have_http_status(201)
+        end
+
+        it "does not assign profesion to the doctor" do
+          expect(Doctor.last.professions).to be_empty
+        end
+
+        it "returns the correct json" do
+          expect(response.body).to eq(User.last.to_json)
+        end
+
+        it "creates a session with the last created user" do
+          expect(controller.current_user).to eq(User.last)
+        end
+      end
+
+      context "with missing profession" do
+        let(:user_attributes) { FactoryBot.attributes_for(:doctor, professions: nil) }
+
+        before do
+          post "/users", params: { user: user_attributes }
+        end
+
+        it "returns a 201 status code" do
+          expect(response).to have_http_status(201)
+        end
+
+        it "does not assign profesion to the doctor" do
+          expect(Doctor.last.professions).to be_empty
+        end
+
+        it "returns the correct json" do
+          expect(response.body).to eq(User.last.to_json)
+        end
+
+        it "creates a session with the last created user" do
+          expect(controller.current_user).to eq(User.last)
+        end
+      end
+
+      context "with mixed valid and invalid professions" do
+        let(:profession) { FactoryBot.create(:profession) }
+        let(:invalid_profession) { "invalid_profession" }
+        let(:user_attributes) { FactoryBot.attributes_for(:doctor, professions: [profession.name, invalid_profession]) }
+
+        before do
+          post "/users", params: { user: user_attributes }
+        end
+
+        it "returns a 201 status code" do
+          expect(response).to have_http_status(201)
+        end
+
+        it "assigns the valid profession to the doctor" do
+          expect(Doctor.last.professions).to eq([profession])
+        end
+
+        it "returns the correct json" do
+          expect(response.body).to eq(User.last.to_json)
+        end
+
+        it "creates a session with the last created user" do
+          expect(controller.current_user).to eq(User.last)
+        end
+      end
+    end
+
+    context "as a patient" do
+      context "with provided valid profession" do
+        let(:profession) { FactoryBot.create(:profession) }
+        let(:user_attributes) { FactoryBot.attributes_for(:patient, professions: [profession.name]) }
+
+        before do
+          post "/users", params: { user: user_attributes }
+        end
+
+        it "returns a 201 status code" do
+          expect(response).to have_http_status(201)
+        end
+
+        it "does not assign the profession to the patient" do
+          expect(profession.doctors).to be_empty
+        end
+
+        it "returns the correct json" do
+          expect(response.body).to eq(User.last.to_json)
+        end
+
+        it "creates a session with the last created user" do
+          expect(controller.current_user).to eq(User.last)
+        end
       end
     end
   end
