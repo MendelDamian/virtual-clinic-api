52d6afb19ae16d373d24a8e6a335f9190df7aada
Damian Mendel <mendeldamian01@gmail.com>
2023-04-27 21:56:34 +0200

    authenticate user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include Warden
+
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user!
+    end
+
+    protected
+
+    def find_verified_user!
+      token = request.headers["Authorization"].split(" ").second
+      decoder = JWTAuth::UserDecoder.new
+      decoder.call(token, :user, nil)
+    rescue StandardError => e
+      reject_unauthorized_connection
+    end
   end
 end
