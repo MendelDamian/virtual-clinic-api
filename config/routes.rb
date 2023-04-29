f710eda11af124f2152786c743c493db34ca81e9
Damian Mendel <mendeldamian01@gmail.com>
2023-04-29 22:34:43 +0200

    add route for chat controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       resources :professions, only: %i[index create]
       resources :work_plans, only: %i[create update destroy]
       resources :procedures, only: %i[index create update destroy]
+      resources :chats, only: %i[index create]
       resources :appointments, only: %i[index create] do
         get 'availability', on: :collection
         get 'cancellation', on: :member
