a5af52dcdc231e763b1910ccb0c8d05803f45ef4
Damian Mendel <mendeldamian01@gmail.com>
2023-08-16 13:21:07 +0200

    merge consecutive work plans

--- app/models/work_plan.rb
+++ app/models/work_plan.rb
@@ -1,6 +1,8 @@
 class WorkPlan < ApplicationRecord
   belongs_to :doctor, inverse_of: :work_plans, foreign_key: :user_id
 
+  before_save :merge_consecutive_work_plans
+
   enum day_of_week: {
     sunday: 0,
     monday: 1,
@@ -18,9 +20,30 @@ class WorkPlan < ApplicationRecord
   include ActiveModel::Validations
   validates_with WorkPlanOverlapValidator
 
+  scope :filter_by_wday, -> (wday) { where(day_of_week: wday) }
+
   def day_of_week=(value)
     self[:day_of_week] = value
   rescue ArgumentError
     self[:day_of_week] = nil
+  end
+
+  private
+
+  def merge_consecutive_work_plans
+    previous_work_plan = doctor.work_plans.filter_by_wday(day_of_week).find_by(work_hour_end: work_hour_start)
+    next_work_plan = doctor.work_plans.filter_by_wday(day_of_week).find_by(work_hour_start: work_hour_end)
+
+    if previous_work_plan && next_work_plan
+      self.update(work_hour_start: previous_work_plan.work_hour_start, work_hour_end: next_work_plan.work_hour_end)
+      previous_work_plan.destroy
+      next_work_plan.destroy
+    elsif previous_work_plan
+      self.update(work_hour_start: previous_work_plan.work_hour_start)
+      previous_work_plan.destroy
+    elsif next_work_plan
+      self.update(work_hour_end: next_work_plan.work_hour_end)
+      next_work_plan.destroy
+    end
   end
 end
