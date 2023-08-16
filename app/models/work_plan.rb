9d2d823f2d446316a1fa8d1a25179c0fd518351b
Damian Mendel <mendeldamian01@gmail.com>
2023-08-16 11:12:57 +0200

    allow multiple schedules per one day

--- app/models/work_plan.rb
+++ app/models/work_plan.rb
@@ -11,7 +11,6 @@ class WorkPlan < ApplicationRecord
     saturday: 6
   }, _prefix: true
 
-  validates_uniqueness_of :day_of_week, scope: :user_id
   validates_inclusion_of :day_of_week, in: day_of_weeks.keys, message: 'is not a valid day of week'
   validates :work_hour_start, :work_hour_end, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }
   validates :work_hour_start, numericality: { less_than: :work_hour_end, message: 'must be before work hour end' }
