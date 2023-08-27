c7d555ec9113d36f8c4e0118feb21c6e1a337c67
Blindeen <kuba.szmajda@gmail.com>
2023-08-27 17:38:44 +0200

    configure s3 active storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Amazon storage
+  config.active_storage.service = :amazon
 end
